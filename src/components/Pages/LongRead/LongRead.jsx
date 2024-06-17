83b60a8888b7a8a7f7cf292584dadfd14e63abff
shest1kk <tux703404@gmail.com>
2024-06-17 23:44:39 +0300

    123

--- src/components/Pages/LongRead/LongRead.jsx
+++ src/components/Pages/LongRead/LongRead.jsx
@@ -2,22 +2,107 @@ import React from "react";
 import './LongRead.css';
 const LongRead = () => {
     return (
-        <div className={LongRead}>
-            <div className='FirstPage'>
+        <div className={'LongRead'}>
             <img 
                     className="FirstPage-img"
                     src={require('../../../assets/1.jpg')}
                     alt="Лонгрид"
                 />
-            </div>
 
-            <div className='SecondPage'>
             <img 
                     className="SecondPage-img"
                     src={require('../../../assets/2.jpg')}
                     alt="Лонгрид"
                 />
-            </div>
+
+            <img 
+                    className="ThirdPage-img"
+                    src={require('../../../assets/3.jpg')}
+                    alt="Лонгрид"
+                />
+
+            <img 
+                    className="FourthPage-img"
+                    src={require('../../../assets/4.jpg')}
+                    alt="Лонгрид"
+                />
+                
+            <img 
+                    className="FivthPage-img"
+                    src={require('../../../assets/5.jpg')}
+                    alt="Лонгрид"
+                />
+
+            <img 
+                    className="SixthPage-img"
+                    src={require('../../../assets/6.jpg')}
+                    alt="Лонгрид"
+                />
+
+            <img 
+                    className="SeventhPage-img"
+                    src={require('../../../assets/7.jpg')}
+                    alt="Лонгрид"
+                />
+
+            <img 
+                    className="EightPage-img"
+                    src={require('../../../assets/8.jpg')}
+                    alt="Лонгрид"
+                />
+
+            <img 
+                    className="NinePage-img"
+                    src={require('../../../assets/9.jpg')}
+                    alt="Лонгрид"
+                />
+
+            <img 
+                    className="TenPage-img"
+                    src={require('../../../assets/10.jpg')}
+                    alt="Лонгрид"
+                />
+
+            <img 
+                    className="ElevenPage-img"
+                    src={require('../../../assets/11.jpg')}
+                    alt="Лонгрид"
+                />
+
+            <img 
+                    className="TwelvePage-img"
+                    src={require('../../../assets/12.jpg')}
+                    alt="Лонгрид"
+                />
+
+            <img 
+                    className="ThirdteenPage-img"
+                    src={require('../../../assets/13.jpg')}
+                    alt="Лонгрид"
+                />
+
+            <img 
+                    className="FourthreenPage-img"
+                    src={require('../../../assets/14.jpg')}
+                    alt="Лонгрид"
+                />
+
+            <img 
+                    className="FivthteenPage-img"
+                    src={require('../../../assets/15.jpg')}
+                    alt="Лонгрид"
+                />
+
+
+
+
+
+
+
+
+
+
+
 
         </div>
         
