c67e69b9522162456461611712a88318e42dbb9a
shest1kk <tux703404@gmail.com>
2024-06-17 16:34:38 +0300

    123

--- src/components/Pages/StartPage/StartPage.jsx
+++ src/components/Pages/StartPage/StartPage.jsx
@@ -8,20 +8,20 @@ const StartPage = () => {
     const { user } = useTelegram();
     
     return (
-        <div className="startPage">
-        <div className="startPage-wrapper">
-            <img 
-                className="startPage-logo"
-                src={require('../../../assets/logo.png')}
-                alt="Логотип ЭйКей"
-            />
-            <div className="startPage-username">Привет, {user?.first_name}!</div>
-            <Link to="/sections">
-                <Button className="primary-button">
-                    Перейти к разделам
-                </Button>
-            </Link>
-        </div>
+        <div className={"startPage"}>
+            <div className={"startPage-wrapper"}>
+                <img 
+                    className={"startPage-logo"}
+                    src={require('../../../assets/logo.png')}
+                    alt="Логотип ЭйКей"
+                />
+                <div className={'startPage-username'}>Привет, {user?.first_name}!</div>
+                <Link to={"/sections"}>
+                    <Button className={'primary-button'}>
+                        Перейти к разделам
+                    </Button>
+                </Link>
+            </div>
         </div>
     );
 }
