8a874ee0c6c05392803f79ac00478fc749d3a1bc
shest1kk <tux703404@gmail.com>
2024-06-17 12:38:39 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useEffect } from 'react';
 import Header from "./components/Header/Header"
 import { useTelegram } from './hooks/useTelegram';
+
 import './App.css';
 
 function App() {
+
+  const {tg} = useTelegram();
 
   useEffect( () => {
     tg.ready();
