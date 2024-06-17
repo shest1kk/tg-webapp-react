b6b6ba76b9ef0ad9a48f9fb7d666d336e404d846
shest1kk <tux703404@gmail.com>
2024-06-17 17:56:20 +0300

    123

--- src/components/Pages/StartPage/StartPage.jsx
+++ src/components/Pages/StartPage/StartPage.jsx
@@ -3,27 +3,31 @@ import './StartPage.css';
 import { useTelegram } from "../../../hooks/useTelegram";
 import Button from "../../Buttons/Button";
 import { Link } from "react-router-dom";
+import { useMediaQuery } from 'react-responsive';
 
 const StartPage = () => {
     const { user } = useTelegram();
-    
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
     return (
-        <div className={"startPage"}>
-            <div className={'startPage-logo'}>
-            <img 
-                    className={"startPage-logo"}
+        <div className="startPage">
+            <div className="startPage-logo">
+                <img 
+                    className="startPage-logo-img"
                     src={require('../../../assets/logo.png')}
                     alt="Логотип ЭйКей"
                 />
             </div>
-            <div className={"startPage-wrapper"}>
-                <div className={'startPage-username'}><b>Привет, {user?.first_name}!</b></div>
-                <div className={'startPage-description'}>Здесь ты можешь узнать подробную информацию о мероприятии SSM2024</div>
-                <Link to={"/sections"}>
-                    <Button className={'primary-button'}>
-                        Перейти к разделам
-                    </Button>
-                </Link>
+            <div className="startPage-wrapper">
+                <div className="startPage-username"><b>Привет, {user?.first_name}!</b></div>
+                <div className="startPage-description">Здесь ты можешь узнать подробную информацию о мероприятии SSM2024</div>
+                {isMobile && 
+                    <Link to="/sections">
+                        <Button className="primary-button">
+                            Перейти к разделам
+                        </Button>
+                    </Link>
+                }
             </div>
         </div>
     );
