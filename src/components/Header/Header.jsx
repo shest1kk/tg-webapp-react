0558437f3f3207a626d8611411cc10ef3fc0ffc1
shest1kk <tux703404@gmail.com>
2024-06-17 12:49:49 +0300

    1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import './Header.css';
 import { useTelegram } from "../../hooks/useTelegram";
 
+
 const Header = () => {
     const {user} = useTelegram();
     
     return (
         <div className={'header'}>
             <img className={'header-logo'}
-            src='../../../public/logo192.png'
-            alt='Логотип ЭйКей'></img>
+            src={require('../../assets/logo192.png')}
+            alt='Логотип ЭйКей'/>
             <span className={'header-username'}>
                 Привет, {user?.username}!
             </span>
