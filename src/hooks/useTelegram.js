46def566e708f089560a8dea40658ba3ab26dea1
shest1kk <tux703404@gmail.com>
2024-08-19 14:15:23 +0300

    bone

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,8 +1,11 @@
 const tg = window.Telegram.WebApp;
+tg.isExpanded;
+tg.expand();
+tg.enableClosingConfirmation();
 
-export function useTelegram () {
+export function useTelegram() {
     return {
         tg,
         user: tg.initDataUnsafe?.user,
-    }
+    };
 }
